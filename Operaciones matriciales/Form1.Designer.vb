e62d7b88e1db2c15697c94efed22b7fc2a1c2e52
zeris <brandon-gg1@outlook.com>
2018-10-21 18:32:16 -0500

    Initial commit

---
+++ Operaciones matriciales/Form1.Designer.vb
@@ -0,0 +1,432 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.a33 = New System.Windows.Forms.TextBox()
+        Me.a32 = New System.Windows.Forms.TextBox()
+        Me.a31 = New System.Windows.Forms.TextBox()
+        Me.a23 = New System.Windows.Forms.TextBox()
+        Me.a22 = New System.Windows.Forms.TextBox()
+        Me.a21 = New System.Windows.Forms.TextBox()
+        Me.a13 = New System.Windows.Forms.TextBox()
+        Me.a12 = New System.Windows.Forms.TextBox()
+        Me.a11 = New System.Windows.Forms.TextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.b33 = New System.Windows.Forms.TextBox()
+        Me.b32 = New System.Windows.Forms.TextBox()
+        Me.b31 = New System.Windows.Forms.TextBox()
+        Me.b23 = New System.Windows.Forms.TextBox()
+        Me.b22 = New System.Windows.Forms.TextBox()
+        Me.b21 = New System.Windows.Forms.TextBox()
+        Me.b13 = New System.Windows.Forms.TextBox()
+        Me.b12 = New System.Windows.Forms.TextBox()
+        Me.b11 = New System.Windows.Forms.TextBox()
+        Me.operando = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.r33 = New System.Windows.Forms.TextBox()
+        Me.r32 = New System.Windows.Forms.TextBox()
+        Me.r31 = New System.Windows.Forms.TextBox()
+        Me.r23 = New System.Windows.Forms.TextBox()
+        Me.r22 = New System.Windows.Forms.TextBox()
+        Me.r21 = New System.Windows.Forms.TextBox()
+        Me.r13 = New System.Windows.Forms.TextBox()
+        Me.r12 = New System.Windows.Forms.TextBox()
+        Me.r11 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.btnLimpiar = New System.Windows.Forms.Button()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.a33)
+        Me.GroupBox1.Controls.Add(Me.a32)
+        Me.GroupBox1.Controls.Add(Me.a31)
+        Me.GroupBox1.Controls.Add(Me.a23)
+        Me.GroupBox1.Controls.Add(Me.a22)
+        Me.GroupBox1.Controls.Add(Me.a21)
+        Me.GroupBox1.Controls.Add(Me.a13)
+        Me.GroupBox1.Controls.Add(Me.a12)
+        Me.GroupBox1.Controls.Add(Me.a11)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 52)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(191, 201)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Matriz a"
+        '
+        'a33
+        '
+        Me.a33.Location = New System.Drawing.Point(143, 158)
+        Me.a33.Name = "a33"
+        Me.a33.Size = New System.Drawing.Size(30, 31)
+        Me.a33.TabIndex = 8
+        '
+        'a32
+        '
+        Me.a32.Location = New System.Drawing.Point(77, 158)
+        Me.a32.Name = "a32"
+        Me.a32.Size = New System.Drawing.Size(30, 31)
+        Me.a32.TabIndex = 7
+        '
+        'a31
+        '
+        Me.a31.Location = New System.Drawing.Point(6, 158)
+        Me.a31.Name = "a31"
+        Me.a31.Size = New System.Drawing.Size(30, 31)
+        Me.a31.TabIndex = 6
+        '
+        'a23
+        '
+        Me.a23.Location = New System.Drawing.Point(143, 97)
+        Me.a23.Name = "a23"
+        Me.a23.Size = New System.Drawing.Size(30, 31)
+        Me.a23.TabIndex = 5
+        '
+        'a22
+        '
+        Me.a22.Location = New System.Drawing.Point(77, 97)
+        Me.a22.Name = "a22"
+        Me.a22.Size = New System.Drawing.Size(30, 31)
+        Me.a22.TabIndex = 4
+        '
+        'a21
+        '
+        Me.a21.Location = New System.Drawing.Point(6, 97)
+        Me.a21.Name = "a21"
+        Me.a21.Size = New System.Drawing.Size(30, 31)
+        Me.a21.TabIndex = 3
+        '
+        'a13
+        '
+        Me.a13.Location = New System.Drawing.Point(143, 30)
+        Me.a13.Name = "a13"
+        Me.a13.Size = New System.Drawing.Size(30, 31)
+        Me.a13.TabIndex = 2
+        '
+        'a12
+        '
+        Me.a12.Location = New System.Drawing.Point(77, 30)
+        Me.a12.Name = "a12"
+        Me.a12.Size = New System.Drawing.Size(30, 31)
+        Me.a12.TabIndex = 1
+        '
+        'a11
+        '
+        Me.a11.Location = New System.Drawing.Point(6, 30)
+        Me.a11.Name = "a11"
+        Me.a11.Size = New System.Drawing.Size(30, 31)
+        Me.a11.TabIndex = 0
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.b33)
+        Me.GroupBox2.Controls.Add(Me.b32)
+        Me.GroupBox2.Controls.Add(Me.b31)
+        Me.GroupBox2.Controls.Add(Me.b23)
+        Me.GroupBox2.Controls.Add(Me.b22)
+        Me.GroupBox2.Controls.Add(Me.b21)
+        Me.GroupBox2.Controls.Add(Me.b13)
+        Me.GroupBox2.Controls.Add(Me.b12)
+        Me.GroupBox2.Controls.Add(Me.b11)
+        Me.GroupBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.Location = New System.Drawing.Point(321, 52)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(191, 201)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Matriz b"
+        '
+        'b33
+        '
+        Me.b33.Location = New System.Drawing.Point(138, 158)
+        Me.b33.Name = "b33"
+        Me.b33.Size = New System.Drawing.Size(30, 31)
+        Me.b33.TabIndex = 9
+        '
+        'b32
+        '
+        Me.b32.Location = New System.Drawing.Point(83, 158)
+        Me.b32.Name = "b32"
+        Me.b32.Size = New System.Drawing.Size(30, 31)
+        Me.b32.TabIndex = 8
+        '
+        'b31
+        '
+        Me.b31.Location = New System.Drawing.Point(18, 158)
+        Me.b31.Name = "b31"
+        Me.b31.Size = New System.Drawing.Size(30, 31)
+        Me.b31.TabIndex = 7
+        '
+        'b23
+        '
+        Me.b23.Location = New System.Drawing.Point(138, 97)
+        Me.b23.Name = "b23"
+        Me.b23.Size = New System.Drawing.Size(30, 31)
+        Me.b23.TabIndex = 6
+        '
+        'b22
+        '
+        Me.b22.Location = New System.Drawing.Point(83, 97)
+        Me.b22.Name = "b22"
+        Me.b22.Size = New System.Drawing.Size(30, 31)
+        Me.b22.TabIndex = 5
+        '
+        'b21
+        '
+        Me.b21.Location = New System.Drawing.Point(18, 97)
+        Me.b21.Name = "b21"
+        Me.b21.Size = New System.Drawing.Size(30, 31)
+        Me.b21.TabIndex = 4
+        '
+        'b13
+        '
+        Me.b13.Location = New System.Drawing.Point(138, 30)
+        Me.b13.Name = "b13"
+        Me.b13.Size = New System.Drawing.Size(30, 31)
+        Me.b13.TabIndex = 3
+        '
+        'b12
+        '
+        Me.b12.Location = New System.Drawing.Point(83, 30)
+        Me.b12.Name = "b12"
+        Me.b12.Size = New System.Drawing.Size(30, 31)
+        Me.b12.TabIndex = 2
+        '
+        'b11
+        '
+        Me.b11.Location = New System.Drawing.Point(18, 30)
+        Me.b11.Name = "b11"
+        Me.b11.Size = New System.Drawing.Size(30, 31)
+        Me.b11.TabIndex = 1
+        '
+        'operando
+        '
+        Me.operando.Location = New System.Drawing.Point(244, 149)
+        Me.operando.Name = "operando"
+        Me.operando.Size = New System.Drawing.Size(37, 20)
+        Me.operando.TabIndex = 8
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(209, 110)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(107, 25)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "Operando"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.r33)
+        Me.GroupBox3.Controls.Add(Me.r32)
+        Me.GroupBox3.Controls.Add(Me.r31)
+        Me.GroupBox3.Controls.Add(Me.r23)
+        Me.GroupBox3.Controls.Add(Me.r22)
+        Me.GroupBox3.Controls.Add(Me.r21)
+        Me.GroupBox3.Controls.Add(Me.r13)
+        Me.GroupBox3.Controls.Add(Me.r12)
+        Me.GroupBox3.Controls.Add(Me.r11)
+        Me.GroupBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox3.Location = New System.Drawing.Point(601, 52)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(191, 201)
+        Me.GroupBox3.TabIndex = 10
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Matriz resultante"
+        '
+        'r33
+        '
+        Me.r33.Location = New System.Drawing.Point(138, 158)
+        Me.r33.Name = "r33"
+        Me.r33.Size = New System.Drawing.Size(30, 31)
+        Me.r33.TabIndex = 9
+        '
+        'r32
+        '
+        Me.r32.Location = New System.Drawing.Point(83, 158)
+        Me.r32.Name = "r32"
+        Me.r32.Size = New System.Drawing.Size(30, 31)
+        Me.r32.TabIndex = 8
+        '
+        'r31
+        '
+        Me.r31.Location = New System.Drawing.Point(18, 158)
+        Me.r31.Name = "r31"
+        Me.r31.Size = New System.Drawing.Size(30, 31)
+        Me.r31.TabIndex = 7
+        '
+        'r23
+        '
+        Me.r23.Location = New System.Drawing.Point(138, 97)
+        Me.r23.Name = "r23"
+        Me.r23.Size = New System.Drawing.Size(30, 31)
+        Me.r23.TabIndex = 6
+        '
+        'r22
+        '
+        Me.r22.Location = New System.Drawing.Point(83, 97)
+        Me.r22.Name = "r22"
+        Me.r22.Size = New System.Drawing.Size(30, 31)
+        Me.r22.TabIndex = 5
+        '
+        'r21
+        '
+        Me.r21.Location = New System.Drawing.Point(18, 97)
+        Me.r21.Name = "r21"
+        Me.r21.Size = New System.Drawing.Size(30, 31)
+        Me.r21.TabIndex = 4
+        '
+        'r13
+        '
+        Me.r13.Location = New System.Drawing.Point(138, 30)
+        Me.r13.Name = "r13"
+        Me.r13.Size = New System.Drawing.Size(30, 31)
+        Me.r13.TabIndex = 3
+        '
+        'r12
+        '
+        Me.r12.Location = New System.Drawing.Point(83, 30)
+        Me.r12.Name = "r12"
+        Me.r12.Size = New System.Drawing.Size(30, 31)
+        Me.r12.TabIndex = 2
+        '
+        'r11
+        '
+        Me.r11.Location = New System.Drawing.Point(18, 30)
+        Me.r11.Name = "r11"
+        Me.r11.Size = New System.Drawing.Size(30, 31)
+        Me.r11.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(543, 149)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(24, 25)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "="
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalcular.Location = New System.Drawing.Point(241, 289)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalcular.TabIndex = 12
+        Me.btnCalcular.Text = "Calcular"
+        Me.btnCalcular.UseVisualStyleBackColor = True
+        '
+        'btnLimpiar
+        '
+        Me.btnLimpiar.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!)
+        Me.btnLimpiar.Location = New System.Drawing.Point(359, 289)
+        Me.btnLimpiar.Name = "btnLimpiar"
+        Me.btnLimpiar.Size = New System.Drawing.Size(75, 23)
+        Me.btnLimpiar.TabIndex = 13
+        Me.btnLimpiar.Text = "Limpiar"
+        Me.btnLimpiar.UseVisualStyleBackColor = True
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!)
+        Me.btnSalir.Location = New System.Drawing.Point(492, 289)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(75, 23)
+        Me.btnSalir.TabIndex = 14
+        Me.btnSalir.Text = "Cerrar"
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(804, 354)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.btnLimpiar)
+        Me.Controls.Add(Me.btnCalcular)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.operando)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents a33 As TextBox
+    Friend WithEvents a32 As TextBox
+    Friend WithEvents a31 As TextBox
+    Friend WithEvents a23 As TextBox
+    Friend WithEvents a22 As TextBox
+    Friend WithEvents a21 As TextBox
+    Friend WithEvents a13 As TextBox
+    Friend WithEvents a12 As TextBox
+    Friend WithEvents a11 As TextBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents b33 As TextBox
+    Friend WithEvents b32 As TextBox
+    Friend WithEvents b31 As TextBox
+    Friend WithEvents b23 As TextBox
+    Friend WithEvents b22 As TextBox
+    Friend WithEvents b21 As TextBox
+    Friend WithEvents b13 As TextBox
+    Friend WithEvents b12 As TextBox
+    Friend WithEvents b11 As TextBox
+    Friend WithEvents operando As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents r33 As TextBox
+    Friend WithEvents r32 As TextBox
+    Friend WithEvents r31 As TextBox
+    Friend WithEvents r23 As TextBox
+    Friend WithEvents r22 As TextBox
+    Friend WithEvents r21 As TextBox
+    Friend WithEvents r13 As TextBox
+    Friend WithEvents r12 As TextBox
+    Friend WithEvents r11 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnCalcular As Button
+    Friend WithEvents btnLimpiar As Button
+    Friend WithEvents btnSalir As Button
+End Class
---
+++ Operaciones matriciales/Form1.vb
@@ -0,0 +1,93 @@
+﻿Public Class Form1
+    Dim a(9), b(9), r(9) As Single
+
+    Private Sub btnLimpiar_Click(sender As Object, e As EventArgs) Handles btnLimpiar.Click
+        r11.Text = ""
+        r12.Text = ""
+        r13.Text = ""
+        r21.Text = ""
+        r22.Text = ""
+        r23.Text = ""
+        r31.Text = ""
+        r32.Text = ""
+        r33.Text = ""
+        a11.Text = ""
+        a12.Text = ""
+        a13.Text = ""
+        a21.Text = ""
+        a22.Text = ""
+        a23.Text = ""
+        a31.Text = ""
+        a32.Text = ""
+        a33.Text = ""
+        b11.Text = ""
+        b12.Text = ""
+        b13.Text = ""
+        b21.Text = ""
+        b22.Text = ""
+        b23.Text = ""
+        b31.Text = ""
+        b32.Text = ""
+        b33.Text = ""
+    End Sub
+
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        Close()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        a(0) = a11.Text
+        a(1) = a12.Text
+        a(2) = a13.Text
+        a(3) = a21.Text
+        a(4) = a22.Text
+        a(5) = a23.Text
+        a(6) = a31.Text
+        a(7) = a32.Text
+        a(8) = a33.Text
+        b(0) = b11.Text
+        b(1) = b12.Text
+        b(2) = b13.Text
+        b(3) = b21.Text
+        b(4) = b22.Text
+        b(5) = b23.Text
+        b(6) = b31.Text
+        b(7) = b32.Text
+        b(8) = b33.Text
+        If operando.Text = "+" Then
+            r11.Text = a(0) + b(0)
+            r12.Text = a(1) + b(1)
+            r13.Text = a(2) + b(2)
+            r21.Text = a(3) + b(3)
+            r22.Text = a(4) + b(4)
+            r23.Text = a(5) + b(5)
+            r31.Text = a(6) + b(6)
+            r32.Text = a(7) + b(7)
+            r33.Text = a(8) + b(8)
+        ElseIf operando.Text = "-" Then
+            r11.Text = a(0) - b(0)
+            r12.Text = a(1) - b(1)
+            r13.Text = a(2) - b(2)
+            r21.Text = a(3) - b(3)
+            r22.Text = a(4) - b(4)
+            r23.Text = a(5) - b(5)
+            r31.Text = a(6) - b(6)
+            r32.Text = a(7) - b(7)
+            r33.Text = a(8) - b(8)
+        ElseIf operando.Text = "*" Then
+            r11.Text = a(0) * b(0)
+            r12.Text = a(1) * b(3)
+            r13.Text = a(2) * b(6)
+            r21.Text = a(3) * b(1)
+            r22.Text = a(4) * b(4)
+            r23.Text = a(5) * b(7)
+            r31.Text = a(6) * b(2)
+            r32.Text = a(7) * b(5)
+            r33.Text = a(8) * b(8)
+        End If
+    End Sub
+End Class
